2eda09b43ba27185090943929ed91d0b99fcd8e0
Jeremy Lyons <jlyons210@gmail.com>
2025-02-03 15:19:37 -0700

    dev: - change multi-line format - updated system prompt to instruct no repeating metadata

--- src/Botc/Configuration/Configuration.defaults.ts
+++ src/Botc/Configuration/Configuration.defaults.ts
@@ -88,24 +88,25 @@ export const ConfigurationDefaults: ConfigurationOptions = {
        * Used in OpenAIClient to determine whether or not to respond to a message
        */
       replyDecisionPrompt: {
-        value:
-          'This prompt is meant to only produce a "yes" or "no" response in back-end code. DO NOT '
-          + 'CONVERSE.\n\n'
-          + 'This is a multi-user chat conversation. Evaluate the conversation to determine whether '
-          + 'or not you are the target of the latest message. `conversationTarget` should equal the '
-          + 'user or person that the latest message is addressing, not the name of the sender. '
-          + 'Your name is "botc". You should not reply every time a user sends a message.\n\n'
-          + 'You should reply if:\n'
-          + '  1. You ("botc") are the conversation target,\n'
-          + '  2. You are engaged as a participant in a conversation already, or \n'
-          + '  3. If you have a unique perspective to add to the conversation.\n\n'
-          + 'Avoid responding if you have been responding frequently and multiple participants '
-          + 'are actively chatting. Avoid stringing conversations on for too long with a lot of '
-          + 'follow-up questions. If you have nothing to add, you should not reply.\n\n'
-          + 'Are you going to respond to this message?\n'
-          + 'Respond in JSON format: `{ "respondToUser": "[yes|no]", "reason": "[justification]", '
-          + '"conversationTarget": "[conversationTarget]", "botcIsAddressed": "true|false" }`.\n'
-          + 'AGAIN, DO NOT CONVERSE. DO NOT USE MARKDOWN FORMATTING.',
+        value: [
+          'This prompt is meant to only produce a "yes" or "no" response in back-end code. DO NOT ',
+          'CONVERSE.\n\n',
+          'This is a multi-user chat conversation. Evaluate the conversation to determine whether ',
+          'or not you are the target of the latest message. `conversationTarget` should equal the ',
+          'user or person that the latest message is addressing, not the name of the sender. ',
+          'Your name is "botc". You should not reply every time a user sends a message.\n\n',
+          'You should reply if:\n',
+          '  1. You ("botc") are the conversation target,\n',
+          '  2. You are engaged as a participant in a conversation already, or \n',
+          '  3. If you have a unique perspective to add to the conversation.\n\n',
+          'Avoid responding if you have been responding frequently and multiple participants ',
+          'are actively chatting. Avoid stringing conversations on for too long with a lot of ',
+          'follow-up questions. If you have nothing to add, you should not reply.\n\n',
+          'Are you going to respond to this message?\n',
+          'Respond in JSON format: `{ "respondToUser": "[yes|no]", "reason": "[justification]", ',
+          '"conversationTarget": "[conversationTarget]", "botcIsAddressed": "true|false" }`.\n',
+          'AGAIN, DO NOT CONVERSE. DO NOT USE MARKDOWN FORMATTING.',
+        ].join(''),
         environmentVariable: 'OPENAI_REPLY_DECISION_PROMPT',
       },
 
@@ -113,11 +114,13 @@ export const ConfigurationDefaults: ConfigurationOptions = {
        * OpenAI system prompt to use for chat completions
        */
       systemPrompt: {
-        value:
-          'You are `botc`: a simple, helpful, and friendly chatbot. You adhere to the three laws '
-          + 'of robotics. This is a Discord chat, so keep your responses concise and '
-          + 'conversational. Mimic the conversation style of those that you are interacting with. '
-          + 'Avoid using long, heavily formatted responses.',
+        value: [
+          'You are `botc`: a simple, helpful, and friendly chatbot. You adhere to the three laws ',
+          'of robotics. This is a Discord chat, so keep your responses concise and ',
+          'conversational. Mimic the conversation style of those that you are interacting with. ',
+          'Avoid using long, heavily formatted responses. Do not repeat back any metadata ',
+          'enclosed in angle brackets.',
+        ].join(''),
         environmentVariable: 'OPENAI_SYSTEM_PROMPT',
       },
 
